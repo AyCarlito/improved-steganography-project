e72c2aa9300301e4295d9f02bf32c38d841cc912
Daniel <2317990h@student.gla.ac.uk>
2020-10-11 20:15:24 +0100

    Moved bitplane slicing into function

--- src/encode.py
+++ src/encode.py
@@ -2,6 +2,7 @@ import numpy
 import time
 from PIL import Image
 
+
 #Open Images
 print("Opening images")
 vessel = Image.open("goldhill.bmp")
@@ -17,30 +18,46 @@ vessel.close()
 secret.close()
 
 
-#Slice bitplanes of vessel
+#Slice bitplanes
 print("Slicing bitplanes of vessel")
-bitplane_arr = numpy.zeros((vessel_arr.shape[0], vessel_arr.shape[1], 8))
-bitplane_arr[:,:,0] = numpy.copy(vessel_arr)
-
-for i in range(bitplane_arr.shape[0]):
-    for j in range(bitplane_arr.shape[0]):
-        bit_array= numpy.unpackbits(numpy.uint8(bitplane_arr[i,j,0]))
-        for k in range(8):
-            bitplane_arr[i,j,k] = bit_array[k]
+vessel_bitplane_arr = numpy.zeros((vessel_arr.shape[0], vessel_arr.shape[1], 8))
+vessel_bitplane_arr[:,:,0] = numpy.copy(vessel_arr)
 
 print("Slicing bitplanes of secret")
 #Slice bitplanes of secret
 secret_bitplane_arr = numpy.zeros((secret_arr.shape[0], secret_arr.shape[1], 8))
 secret_bitplane_arr[:,:,0] = numpy.copy(secret_arr)
 
-for i in range(secret_bitplane_arr.shape[0]):
-    for j in range(secret_bitplane_arr.shape[0]):
-        secret_bit_array= numpy.unpackbits(numpy.uint8(secret_bitplane_arr[i,j,0]))
-        for k in range(8):
-            secret_bitplane_arr[i,j,k] = secret_bit_array[k]
+def getBitplaneArr(matrix):
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            bit_array = numpy.unpackbits(numpy.uint8(matrix[i,j,0]))
+            for k in range(8):
+                matrix[i,j,k] = bit_array[k]
+    return matrix
+
+vessel_bitplane_arr = getBitplaneArr(vessel_bitplane_arr)
+secret_bitplane_arr = getBitplaneArr(secret_bitplane_arr)
+
+# for i in range(bitplane_arr.shape[0]):
+#     for j in range(bitplane_arr.shape[0]):
+#         bit_array= numpy.unpackbits(numpy.uint8(bitplane_arr[i,j,0]))
+#         for k in range(8):
+#             bitplane_arr[i,j,k] = bit_array[k]
+
+
+
+# for i in range(secret_bitplane_arr.shape[0]):
+#     for j in range(secret_bitplane_arr.shape[0]):
+#         secret_bit_array= numpy.unpackbits(numpy.uint8(secret_bitplane_arr[i,j,0]))
+#         for k in range(8):
+#             secret_bitplane_arr[i,j,k] = secret_bit_array[k]
 
 
 #Chop sceret into 8x8 bitplanes and place in queue
+
+# for i in range(secret_bitplane_arr.shape[0]/8):
+#     for j in range(secret_bitplane_arr.shape[1]/8)
 
 #find complex segements and replace
 
