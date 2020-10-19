9e9f46e0a65f4256b5e0a721a225719afd4a0263
Daniel <2317990h@student.gla.ac.uk>
2020-10-19 19:45:18 +0100

    Steganalysis tool to generate histograms

---
+++ src/steganography/steganalysis.py
@@ -0,0 +1,60 @@
+import numpy as np
+from PIL import Image
+import os
+import encode
+import plotly.graph_objects as go
+
+def complexity(data):
+    complexities = []
+    for block in data:
+        complexities.append(encode.get_complexity(block))
+    return complexities
+
+def plot(x, y, gtitle, xtitle, ytitle):
+    print("Plotting graph")
+    fig = go.Figure()   
+    fig.add_trace(go.Scattergl(x=x, y=y, mode='lines', name='lines'))
+    fig.update_layout(
+        title=gtitle,
+        xaxis_title=xtitle,
+        yaxis_title=ytitle,
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="#7f7f7f"
+        )
+    )
+    fig.show()
+
+def main():
+
+    vessel_arr = encode.get_file("vessel")
+    stego_arr= encode.get_file("stego")
+
+
+    print("Getting binary encoding of vessel")
+    vessel_bitplane_arr = np.zeros((vessel_arr.shape[0], vessel_arr.shape[1], 8))
+    vessel_bitplane_arr[:,:,0] = np.copy(vessel_arr)
+    vessel_bitplane_arr = encode.get_bitplane_arr(vessel_bitplane_arr)
+
+    print("Getting binary encoding of secret")
+    stego_bitplane_arr = np.zeros((stego_arr.shape[0], stego_arr.shape[1], 8))
+    stego_bitplane_arr[:,:,0] = np.copy(stego_arr)
+    stego_bitplane_arr = encode.get_bitplane_arr(stego_bitplane_arr)
+
+    vessel_data = encode.split_into_blocks(vessel_bitplane_arr)
+    stego_data = encode.split_into_blocks(stego_bitplane_arr)
+
+    vessel_complexities = complexity(vessel_data)
+    stego_complexities = complexity(stego_data)
+
+    vessel_pixel_values, vessel_pixel_counts = np.unique(vessel_arr, return_counts=True)
+    stego_pixel_values, stego_pixel_counts = np.unique(stego_arr, return_counts=True)
+
+    plot(vessel_pixel_values, vessel_pixel_counts, "Pixel Value Histogram of Vessel Image", "Pixel values", " Occurences of Values")
+    plot(np.arange(len(vessel_data)), vessel_complexities, "Complexity Histogram of Vessel Image", "8x8 Blocks", "Complexity of Block")
+
+    plot(stego_pixel_values, stego_pixel_counts, "Pixel Value Histogram of Stego Image", "Pixel values", " Occurences of Values")
+    plot(np.arange(len(stego_data)), stego_complexities, "Complexity Histogram of Stego Image", "8x8 Blocks", "Complexity of Block")
+
+main()
