ca843c78b8a940596701cc721e3a085f2157c761
Daniel <2317990h@student.gla.ac.uk>
2020-12-15 00:18:51 +0000

    Random LSP JPEG replacement

--- src/steganography/jpeg_steganography/jpeg_encode.py
+++ src/steganography/jpeg_steganography/jpeg_encode.py
@@ -7,6 +7,7 @@ import csv
 import math
 import os
 import ast
+import lsb_jpeg
 
 LUMINANCE_MATRIX = np.array([
     [16,11,10,16,24,40,51,61],
@@ -35,7 +36,7 @@ def get_arguments():
     parser.add_argument("v", type=str, help="Vessel Image")
     parser.add_argument("s", type=str, help="Secret Image")
     parser.add_argument("q", type=str, help="Quality Factor")
-    parser.add_argument("m", type=str, choices=["LSB"], help="Embedding Algorithm")
+    parser.add_argument("m", type=str, choices=["LSB", "LSBRandom"], help="Embedding Algorithm")
     args = parser.parse_args()
     return args
 
@@ -134,94 +135,18 @@ def handle_channel(img, quantization_matrix):
     remove_rle_file()
     return compressed_arr
 
-
-# def lsb_embed_secret(secret, compressed):
-#     block_count = 0
-#     for i in range(compressed.shape[0]//8):
-#         for j in range(compressed.shape[1]//8):
-#             if block_count<=len(secret):
-#                 block = compressed[i*8:i*8+8, j*8:j*8+8]
-#                 block_count+=1
-#                 for row in range(8):
-#                     secret_to_embed = np.unpackbits(secret[0])
-#                     secret.pop(0)
-#                     for column in range(8):
-#                         binary_repr = np.unpackbits(block[row,column])
-#                         binary_repr[-1] = secret_to_embed[column]
-#                         block[row, column] = np.packbits(binary_repr)
-#                 compressed[i*8:i*8+8, j*8:j*8+8] = block       
-#             else:
-                
-#                 return compressed
-#     return compressed
-
-# def lsb_decode_secret():
-#     img = get_file("compressed.jpeg")
-#     data = create_blocks(img)
-#     secret = []
-#     retrieved = 0
-#     for block in data:
-#         secret_pixel = []
-#         if retrieved<14568:
-#             for row in range(8):
-#                 for column in range(8):
-#                     pixel = np.unpackbits(block[row,column])
-#                     secret_pixel.append(pixel[-1])
-#                 secret.extend(np.packbits(secret_pixel))
-#                 retrieved+=1
-#     return secret
-
-def lsb_embed_secret(secret, vessel):
-    index = 0
-
-    height = vessel.shape[0]
-    width = vessel.shape[1]
-
-    secret_height = secret.shape[0]
-    secret_width = secret.shape[1]
-    secret = secret.flatten()
-    vessel = vessel.flatten()
-
-    for secret_pixel in secret:
-        secret_bin_repr = np.unpackbits(secret_pixel)
-        vessel_selection = vessel[index*8:index*8+8]
-        for i, vessel_pixel in enumerate(vessel_selection):
-            vessel_pixel_bin_repr = np.unpackbits(vessel_pixel)
-            vessel_pixel_bin_repr[-1] = 1
-            vessel_pixel_bin_repr[-2] = 0
-            vessel_pixel_bin_repr[-3] = secret_bin_repr[i]
-            vessel_pixel = np.packbits(vessel_pixel_bin_repr)
-            vessel_selection[i] = vessel_pixel
-        vessel[index*8:index*8+8] = vessel_selection
-        index+=1
-    vessel[0] = (secret_height/8)
-    vessel[8] = (secret_width/8)
-    return vessel.reshape((height,width))
-
-def lsb_decode_secret():
-    stego = get_file("compressed.jpeg").flatten()
-    secret = []
-    index = 0
-
-    height = stego[0]*8
-    width = stego[8]*8
-    for index in range(len(stego)//8):
-        stego_selection = stego[index*8:index*8+8]
-        secret_pixel = []
-        for i, stego_pixel in enumerate(stego_selection):
-            stego_pixel_bin_repr = np.unpackbits(stego_pixel)
-            secret_pixel.append(stego_pixel_bin_repr[-3])
-        secret.append(np.packbits(secret_pixel)[0])
-        index+=1
-    return np.asarray(secret)[0:(height*width)].reshape((height, width))
-    
 def handle_grayscale(vessel, secret, qtmatrix, mode):
     compressed_arr = clean_values(handle_channel(vessel, qtmatrix))
     if mode=="LSB":
-            compressed_arr = lsb_embed_secret(secret, np.uint8(compressed_arr))
-            create_image(compressed_arr, "compressed")
-            recovered = lsb_decode_secret()
-            create_image(recovered, "secret")
+        compressed_arr = lsb_jpeg.lsb_embed_secret(secret, np.uint8(compressed_arr))
+        create_image(compressed_arr, "compressed")
+        recovered = lsb_jpeg.lsb_decode_secret()
+        create_image(recovered, "secret")
+    elif mode=="LSBRandom":
+        compressed_arr = lsb_jpeg.random_lsb_embed_secret(secret, np.uint8(compressed_arr))
+        create_image(compressed_arr, "compressed")
+        recovered = lsb_jpeg.random_lsb_decode_secret()
+        create_image(recovered, "secret")
     else:
         create_image(compressed_arr, "compressed")
     
@@ -229,14 +154,14 @@ def handle_colour(vessel, secret, channel_matrix, mode):
     for i in range(3):
         vessel[:,:,i] = handle_channel(vessel[:,:,i], channel_matrix[i])
     if mode=="LSB":
-        vessel[:,:,0] = lsb_embed_secret(secret, np.uint8(vessel[:,:,0]))
+        vessel[:,:,0] = lsb_jpeg.lsb_embed_secret(secret, np.uint8(vessel[:,:,0]))
         vessel[0,0,1] = vessel[0,0,0]
         vessel[0,0,2] = vessel[0,0,0]
         vessel[0,8,1] = vessel[0,8,0]
         vessel[0,8,2] = vessel[0,8,0]
         compressed_arr = cv2.cvtColor(vessel, cv2.COLOR_YCrCb2BGR)
         create_image(clean_values(compressed_arr), "compressed")
-        recovered = np.asarray(lsb_decode_secret())
+        recovered = np.asarray(lsb_jpeg.lsb_decode_secret())
         create_image(recovered, "secret")
     else:
         compressed_arr = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
